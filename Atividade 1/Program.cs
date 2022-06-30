5aca023356d4a900116c46d6fa2134defc2337fe
Barc0m <103148113+Barc0m@users.noreply.github.com>
2022-06-30 19:13:43 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Em uma tigela coloque o fermento, o açúcar, a água e o azeite e misture bem. \n Coloque uma xícara de farinha e misture, então acrescente o sal. \n Continue adicionando a farinha até obter uma massa que quase não grude nas mãos.\n Coloque -as sobre a bancada levemente enfarinhada e sove por alguns minutos, só para a massa ficar lisa.\n Separe a massa em 3 partes, modele -as ligeiramente, fazendo bolas. \n Coloque em uma forma, cubra com um pano e leve para crescer em local fechado, por 1h a 1h30.Até dobrar de volume.\n Assim que a massa estiver crescida, pegue uma bola de massa, coloque - a sobre a bancada levemente enfarinhada e comece a abrir.Pode ser om as mãos ou com o rolo de masa, trabalhando para manter a massa no formato redondo.Elas ficarão com aproximadamente 30 cm.\n Acomode -as em em formas de pizza.\n Então monte as suas pizzas(veja todas as dicas abaixo).\n Antes de assar, você deve aquecer o forno em temperatura máxima por 30 a 40 minutos.\n Asse -as pizzas até que o queijo esteja derretido e a base levemente dourada.\n As minhas ficam prontas em 10 a 12 minutos no forno doméstico(elétrico e a gás).");
+
+            Console.ReadKey();
+        }
+    }
+}
